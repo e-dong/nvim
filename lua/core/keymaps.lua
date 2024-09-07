ead0bb43122520f5e7b1c69459bb9d47f4908a25
Eric Dong <eric2043@gmail.com>
2024-09-06 22:28:02 -0400

    Added key bindings to yank and paste to clipboard

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,10 @@
 -- General
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
+-- Yank to clipboard with <leader>y
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
+
+-- Paste from clipboard with <leader>p
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p')
+
+
