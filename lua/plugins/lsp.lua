c8c208f5052e91b12c117016999739dcf054c228
Eric Dong <eric2043@gmail.com>
2024-09-06 22:33:51 -0400

    Added keybinding to open the vim diagnostic float window

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,6 +22,7 @@ return {
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {})
       vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
     end
   }
