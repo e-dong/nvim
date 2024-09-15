7623d5ab8a039dead270b913f896ba0aafee06d4
Eric Dong <eric2043@gmail.com>
2024-09-14 23:54:40 -0400

    Added autocommand to disable left column numbers in terminal buffer

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,2 +1,10 @@
 -- terminal mode
 vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	callback = function()
+		vim.opt.number = false
+		vim.wo.relativenumber = false
+		vim.opt.signcolumn = "no"
+	end,
+})
