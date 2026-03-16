a00cdb980ec285ad356a33d01dd8505546208650
Eric Dong <eric2043@gmail.com>
2026-03-16 00:23:55 -0400

    Add transparency handling for diffview

--- lua/plugins/git-diff.lua
+++ lua/plugins/git-diff.lua
@@ -6,5 +6,19 @@ return {
 		vim.keymap.set("n", "<leader>dx", "<cmd>DiffviewClose<CR>", { desc = "Closes git diff view" })
 		vim.keymap.set("n", "<leader>dh", "<cmd>DiffviewFileHistory<CR>", { desc = "Opens git diff file history" })
 		vim.keymap.set("n", "<leader>dr", "<cmd>DiffviewRefresh<CR>", { desc = "Refreshes diff view" })
+
+    -- highlights for transparency
+    local groups = {
+      "DiffChange",
+      "DiffChanged",
+      "diffAdded",
+      "diffRemoved"
+    }
+
+    for _, group in ipairs(groups) do
+        local hl = vim.api.nvim_get_hl(0, {name = group})
+        local fg = hl.fg
+        vim.api.nvim_set_hl(0, group, { bg = "none", fg = fg })
+    end
 	end,
 }
